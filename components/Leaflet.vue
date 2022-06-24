164077fafdaa722f134e5c4ff0a5a13ab0b298be
DAIMER2001 <daimercuellar@gmail.com>
2022-06-24 10:42:37 -0500

    change

---
+++ components/Leaflet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <b-card bg-variant="dark" header="Mapa Neiva" text-variant="white" class="text-center">
+
+        <div id="map-wrap" style="height: 80vh; width: 100%;">
+        <no-ssr>
+            <l-map :zoom="13" :center="[2.9251045,-75.298481]">
+            <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+            <l-marker v-for=" marcador in marcadores " :lat-lng="marcador">
+            <l-popup>Hello!</l-popup>
+            </l-marker>
+            </l-map>
+        </no-ssr>
+        </div>
+
+      </b-card>
+  </div>
+</template>
+<script>
+export default {
+    data()  {
+        return {
+            marcadores: [
+                    [2.9251045,-75.298481],
+                    [2.9270576,-75.2855848],
+            ]
+        }
+    },
+  mounted() {
+    
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+
+    <b-container class="mt-5">
+
+    <Leaflet></Leaflet>
+
+    </b-container>
+  </div>
+</template>
+<script>
+import Leaflet from '../components/Leaflet.vue';
+import Navbar from '../components/Navbar.vue';
+export default {
+    mounted() {
+    },
+    components: { Leaflet, Navbar }
+};
+</script>
